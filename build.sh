77418504ef422a7dfef3d1a930beef955acef4bd
catsanddo <travisscoville@gmail.com>
2022-05-18 16:22:09 -0400

    First commit

---
+++ build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+BINARY='bin/calculator'
+
+usage () {
+    echo "Usage: build.sh [OPTIONS]"
+    echo "-r     run after building"
+    echo "-c     remove build dir"
+    echo "-h     print this help"
+}
+
+while getopts ":rch" option; do
+    case $option in
+        h)
+            usage
+            exit 0
+            ;;
+        r)
+            should_run=yes
+            ;;
+        c)
+            rm -rf bin
+            exit 0
+            ;;
+        *)
+            ;;
+    esac
+done
+
+mkdir -p bin
+odin build src -out:bin/calculator
+
+if [[ $? -eq 0 ]] && [[ -n $should_run ]]; then
+    bin/calculator
+else
+    exit 1
+fi
---
+++ src/calculator.odin
@@ -0,0 +1,7 @@
+package calculator
+
+import "core:fmt"
+
+main :: proc() {
+    fmt.println("Hellope!")
+}
